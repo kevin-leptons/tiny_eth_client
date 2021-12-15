9d9480ef718f417aabf1ccd6145068b6f1f7f322
Kevin Leptons <kevin.leptons@gmail.com>
2021-12-15 00:52:45 -0800

    change AbiInterface to AbiCodec

--- test/abi_codec.js
+++ test/abi_codec.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const assert = require('assert')
-const {AbiInterface} = require('../lib')
+const {AbiCodec} = require('../lib')
 const UniswapV2PoolAbi = require('./_data/uniswap_v2_pool_abi.json')
 
-describe('AbiInterface', () => {
+describe('AbiCodec', () => {
     it('getEventTopic', () => {
-        let iface = new AbiInterface(UniswapV2PoolAbi)
+        let iface = new AbiCodec(UniswapV2PoolAbi)
         let actualResult = iface.getEventTopic('Swap')
         let expectedResult = '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822'
 
