50a076db0954b7408fdc7af9c8b84f49f44cc2f7
Kevin Leptons <kevin.leptons@gmail.com>
2021-12-16 21:42:13 -0800

    change Client.call to Client._call

--- test/diary.js
+++ test/diary.js
@@ -16,16 +16,7 @@ describe('Diary.getLogs', () => {
             'https://bsc-dataseed.binance.org/',
             'https://bsc-dataseed1.defibit.io/',
             'https://bsc-dataseed1.ninicoin.io/',
-            'https://bsc-dataseed2.defibit.io/',
-            'https://bsc-dataseed3.defibit.io/',
-            'https://bsc-dataseed4.defibit.io/',
-            'https://bsc-dataseed2.ninicoin.io/',
-            'https://bsc-dataseed3.ninicoin.io/',
-            'https://bsc-dataseed4.ninicoin.io/',
-            'https://bsc-dataseed1.binance.org/',
-            'https://bsc-dataseed2.binance.org/',
-            'https://bsc-dataseed3.binance.org/',
-            'https://bsc-dataseed4.binance.org/'
+            'https://bsc-dataseed2.defibit.io/'
         ]
         let nodes = endpoints.map((endpoint, index) => {
             return new Node({
